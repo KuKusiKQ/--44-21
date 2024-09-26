d8adb357331364300a036bd10c93065a5d32d7a7
Petrova Valentina <valya@chuvsu.ru>
2024-09-26 16:02:38 +0300

    lab3

---
+++ Ivanov-Leonid-KT-44-21/Database/StudentDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ivanov_Leonid_KT_44_21.Database.Configurations;
+using Ivanov_Leonid_KT_44_21.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ivanov_Leonid_KT_44_21.Database
+{
+    public class StudentDbContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+        DbSet<Group> Groups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupConfiguration());
+        }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Ivanov-Leonid-KT-44-21/Models/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace Ivanov_Leonid_KT_44_21.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+
+        public string? GroupName { get; set; }
+    }
+}
